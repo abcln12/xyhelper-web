0be9d4747738a09fa0cd181fa4fa30b5273f6cb9
Li Dong <cnlidong@live.cn>
2023-04-04 10:31:55 +0800

    暂存

--- modules/chatweb/controller/app/chatweb_api.go
+++ modules/chatweb/controller/app/chatweb_api.go
@@ -2,11 +2,13 @@ package app
 
 import (
 	"context"
+	"time"
 
 	"github.com/cool-team-official/cool-admin-go/cool"
 
 	"github.com/gogf/gf/v2/encoding/gjson"
 	"github.com/gogf/gf/v2/frame/g"
+	chatgpt "github.com/xyhelper/chatgpt-go"
 )
 
 type ChatwebApiController struct {
@@ -68,5 +70,88 @@ func (c *ChatwebApiController) Session(ctx context.Context, req *ChatwebApiSessi
 			Model: "ChatGPTUnofficialProxyAPI",
 		},
 	}
+	return
+}
+
+// ChatProcessReq 请求参数
+type ChatwebApiChatProcessReq struct {
+	g.Meta  `path:"/chat-process-gf" method:"POST"`
+	Prompt  string `json:"prompt"` // 提示
+	Options *struct {
+		ConversationID  string `json:"conversationId"`  // 会话ID
+		ParentMessageId string `json:"parentMessageId"` // 父消息ID
+	} `json:"options"` // 选项
+}
+
+// ChatProcessRes 返回参数
+type ChatwebApiChatProcessRes struct {
+}
+
+// ChatProcess 会话
+func (c *ChatwebApiController) ChatProcess(ctx context.Context, req *ChatwebApiChatProcessReq) (res *ChatwebApiChatProcessRes, err error) {
+	r := g.RequestFromCtx(ctx)
+	token := `random token`
+
+	cli := chatgpt.NewClient(
+		// chatgpt.WithDebug(true),
+		chatgpt.WithTimeout(120*time.Second),
+		chatgpt.WithAccessToken(token),
+		chatgpt.WithBaseURI("https://freechat.lidong.xin"),
+	)
+	var stream *chatgpt.ChatStream
+	if req.Options.ConversationID == "" && req.Options.ParentMessageId == "" {
+		stream, err = cli.GetChatStream(req.Prompt)
+	} else {
+		stream, err = cli.GetChatStream(req.Prompt, req.Options.ConversationID, req.Options.ParentMessageId)
+	}
+	if err != nil {
+		return nil, err
+	}
+	// w := r.Response.RawWriter()
+	// f, ok := w.(http.Flusher)
+	// if !ok {
+	// 	return nil, err
+	// }
+	g.Log().Debug(ctx, "开始写入响应内容")
+	r.Response.Header().Set("Content-Type", "text/event-stream")
+	r.Response.Header().Set("Cache-Control", "no-cache")
+	r.Response.Header().Set("Connection", "keep-alive")
+	r.Response.Flush()
+	for text := range stream.Stream {
+		// msg := gjson.New(text.String())
+		responseData := map[string]interface{}{
+			"role":            "assistant",
+			"id":              text.MessageID,
+			"parentMessageId": req.Options.ParentMessageId,
+			"conversationId":  text.ConversationID,
+			"text":            text.Content,
+			// "time":            time.Now().Unix(),
+		}
+		// fmt.Fprintf(w, "%s\n", gjson.New(responseData).MustToJson())
+		// r.Response.ResponseWriter.Write()
+
+		// r.Response.RawWriter().Write(gconv.Bytes(responseData))
+		// r.Response.RawWriter().Write([]byte("\n"))
+		// r.Response.SetBuffer(gconv.Bytes(responseData))
+		r.Response.WriteJson(responseData)
+
+		r.Response.Writer.Flush()
+
+		// resJson := gjson.New(responseData).MustToJson()
+		// r.Response.WriteJson(responseData)
+		// r.Response.Write([]byte("\n"))
+		// r.Response.Flush()
+		// r.Response.RawWriter().Write([]byte(resJson))
+		// r.Response.Flush()
+		g.Log().Debug(ctx, responseData)
+	}
+	if err := stream.Err; err != nil {
+		g.Log().Error(ctx, err)
+		return nil, err
+	}
+	g.Log().Debug(ctx, "写入响应内容完成")
+	// r.Exit()
+
+	// res = &ChatwebApiChatProcessRes{}
 	return
 }
